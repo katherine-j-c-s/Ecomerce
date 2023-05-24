d2c15584c3f9b9d163a8a16939f1e0278b442e16
Agustin <agustinandrada1@gmail.com>
2023-05-24 13:45:44 -0300

    Deployado de bdd

--- ecomerce/Api/src/models/Product.js
+++ ecomerce/Api/src/models/Product.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "product",
+    "Product",
     {
       id: {
         type: DataTypes.INTEGER,
