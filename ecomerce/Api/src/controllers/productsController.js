73dd683536bdacff85ff2c8ee28c6f0bb9246c90
JMCode97 <doomed2506@gmail.com>
2023-05-24 14:15:19 -0400

    Products controllers

---
+++ ecomerce/Api/src/controllers/productsController.js
@@ -0,0 +1,65 @@
+const { Product } = require('../db')
+
+
+const getProducts = async () => {
+    let products = await Product.findAll()
+
+    return products
+}
+
+const getProductByID = async (id) => {
+    let product = await Product.findOne({
+        where: { id }
+    })
+
+    return product
+}
+
+const createProduct = async (name, price, description, rating, image) => {
+    let [product, created] = await Product.findOrCreate({
+        where: { name },
+        defaults: {
+            name,
+            price,
+            description,
+            rating,
+            image
+        }
+    })
+
+    if(!created) {
+        await Product.increment('stock', {by: 1})
+    }
+
+    return product
+}
+
+const deleteProduct = async (id) => {
+    let removedProduct = await Product.destroy({
+        where: { id }
+    })
+
+    return removedProduct
+}
+
+const updateProduct = async (name, price, description, rating, image) => {
+    let data = {name, price, description, rating, image}
+    let newData = {}
+
+    for(el in data) {
+        if(data[el] !== '' || data[el] !== null || data[el] !== 'undefined') {
+            newData[el] = data[el]
+        }
+    }
+    let updatedProduct = await Product.update({ newData }, { name })
+
+    return updatedProduct
+}
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProductByID,
+    deleteProduct,
+    updateProduct,
+}
--- ecomerce/Api/src/db.js
+++ ecomerce/Api/src/db.js
@@ -10,7 +10,7 @@ const path = require("path");
 // });
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecomerce`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -22,13 +22,13 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
     (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
