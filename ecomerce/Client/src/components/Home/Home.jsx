ba20eb6031a3fa9a42366e67b7eb978c72da05ab
DVRKSKY <amadeo21sept@gmail.com>
2023-05-30 09:51:49 -0500

    fix-background-home

--- ecomerce/Client/src/components/Home/Home.jsx
+++ ecomerce/Client/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import stylesHome from "../Home/Home.css";
+import stylesHome from "../Home/Home.module.css";
 
 import { Link } from "react-router-dom";
 
@@ -6,13 +6,13 @@ const Home = ()=>{
     return (
         <div id={stylesHome.backgroundHome}>
             <article className="m-3 mt-36 mb-14 p-3">
-                <h1 className="text-7xl text-black font-medium">Just buy it</h1>
-                <p className="text-3xl text-black font-light">
+                <h1 className="text-7xl text-white font-medium">Just buy it</h1>
+                <p className="text-3xl text-white font-light">
                 Discover our new styles
                 </p>
             </article>
 
-            <Link to="/alls" className="text-black hover:text-black">
+            <Link to="/alls" className="text-white hover:text-black">
                 <button className="px-20 rounded-sm border-bluey">Buy now</button>
             </Link>
         </div>
