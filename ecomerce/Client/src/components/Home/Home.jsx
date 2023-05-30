005219df9caa6812ae3202762fe97787d4f6c199
katherine-j-c-s <kathijcs@gmail.com>
2023-05-30 12:27:13 -0300

    passed cardsProductos to home component

--- ecomerce/Client/src/components/Home/Home.jsx
+++ ecomerce/Client/src/components/Home/Home.jsx
@@ -1,22 +1,33 @@
 import stylesHome from "../Home/Home.module.css";
-
+import CardsProduct from '../CardsProduct/CardsProduct'
 import { Link } from "react-router-dom";
 
 const Home = ()=>{
     return (
-        <div id={stylesHome.backgroundHome} className="h-screen">
-            <article className="m-3 mt-36 mb-14 p-3">
-                <h1 className="text-7xl text-white font-medium">Just buy it</h1>
-                <p className="text-3xl text-white font-light">
-                Discover our new styles
-                </p>
-            </article>
+        <div>
+            <div id={stylesHome.backgroundHome} className="h-screen">
+                <article className="m-3 mt-36 mb-14 p-3">
+                    <h1 className="text-7xl text-white font-medium">Just buy it</h1>
+                    <p className="text-3xl text-white font-light">
+                    Discover our new styles
+                    </p>
+                </article>
 
-            <Link to="/alls" className="text-white hover:text-black">
-                <button className="px-20 rounded-sm border-bluey">Buy now</button>
-            </Link>
+                <Link to="/alls" className="text-white hover:text-black">
+                    <button className="px-20 rounded-sm border-bluey">Buy now</button>
+                </Link>
+            </div>
+            <div className='h-[70vh] w-auto'></div>
+            <div className='h-[70vh] w-auto'>
+                <h2>New styles just arrive</h2>
+                <div className='flex flex-row gap-4 justify-center items-center'>
+                    <CardsProduct/>
+                    <CardsProduct/>
+                    <CardsProduct/>
+                    <CardsProduct/> 
+                </div>
+            </div>
         </div>
-
     )
 }
 
--- ecomerce/Client/src/views/Home/ViewHome.jsx
+++ ecomerce/Client/src/views/Home/ViewHome.jsx
@@ -1,23 +1,9 @@
 import Home from '../../components/Home/Home';
-import CardsProduct from '../../components/CardsProduct/CardsProduct';
 
-//<Home/>
 const ViewHome =()=>{
     return(
         <div>
             <Home/>
-            <div className='h-[70vh] w-auto'></div>
-
-            <div className='h-[70vh] w-auto'>
-                <h2>New styles just arrive</h2>
-                <div className='flex flex-row gap-4 justify-center items-center'>
-                    <CardsProduct/>
-                    <CardsProduct/>
-                    <CardsProduct/>
-                    <CardsProduct/> 
-                </div>
-            </div>
-            
         </div>
     )
 }
