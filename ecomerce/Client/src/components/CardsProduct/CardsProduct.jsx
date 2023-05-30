f8b9569a85b04427c8f62bd93848535c346d833d
DVRKSKY <amadeo21sept@gmail.com>
2023-05-30 11:32:55 -0500

    fix-cards-details

--- ecomerce/Client/src/components/CardsProduct/CardsProduct.jsx
+++ ecomerce/Client/src/components/CardsProduct/CardsProduct.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-export default function CardsProduct() {
+export default function CardsProduct({image, price, name, addToCart, seeDetails}) {
   return (
     <div className='flex w-72 h-80 rounded-lg bg-white flex-col relative shadow hover:shadow-xl' >
-        <img  className='select-none absolute -top-10 left-1/2 transform -translate-x-1/2 mx-2' src='https://firebasestorage.googleapis.com/v0/b/app-keepers.appspot.com/o/image-PhotoRoom%207.png?alt=media&token=6ea0c1a8-4f4f-454d-8a01-8358012f4f63&_gl=1*1wkznzv*_ga*MTMyNjgzMDA1LjE2ODAyMzYxMDA.*_ga_CW55HF8NVT*MTY4NTQyNTY3NC4zLjEuMTY4NTQyNTc2My4wLjAuMA..' alt='Producto' />
+        <img  className='select-none absolute -top-10 left-1/2 transform -translate-x-1/2 mx-2' src={image} alt={name} />
         <div className='flex flex-row absolute bottom-2 left-1/2 transform -translate-x-1/2 mb-10 space-x-8 w-full justify-center'>
             <div className='items-start h-10 w-24'>
-              <h3 className='text-xl font-bold text-teal-400 text-left '>$1000.00</h3>
-              <span className='text-sm font-light text-gray-600 leading-tight truncate overflow-hidden overflow-ellipsis'>Lorem ipsum dolor</span>
+              <h3 className='text-xl font-bold text-teal-400 text-left '>${price}</h3>
+              <span className='text-sm font-light text-gray-600 leading-tight truncate overflow-hidden overflow-ellipsis'>{name}</span>
             </div>
             <div className='flex flex-row justify-content items-center'>
-              <div className='flex justify-center items-center rounded-full bg-teal-400 w-9 h-9 cursor-pointer mx-1'>
+              <div onClick={()=> addToCart()} className='flex justify-center items-center rounded-full bg-teal-400 w-9 h-9 cursor-pointer mx-1'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
               </div>
-              <div className='flex justify-center items-center rounded-full bg-zinc-200 text-teal-400 w-9 h-9 cursor-pointer mx-1'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              <div onClick={seeDetails} className='flex justify-center items-center rounded-full bg-zinc-200 text-teal-400 w-9 h-9 cursor-pointer mx-1'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
               </div>
             </div>
--- ecomerce/Client/src/views/Home/ViewHome.jsx
+++ ecomerce/Client/src/views/Home/ViewHome.jsx
@@ -1,20 +1,47 @@
 import Home from '../../components/Home/Home';
 import CardsProduct from '../../components/CardsProduct/CardsProduct';
+import { useNavigate } from 'react-router-dom'
+
+const products = [
+    {id:1,name: 'Zapatillas nike negras', price: 1000, image:'https://firebasestorage.googleapis.com/v0/b/app-keepers.appspot.com/o/image-PhotoRoom%207.png?alt=media&token=6ea0c1a8-4f4f-454d-8a01-8358012f4f63&_gl=1*1wkznzv*_ga*MTMyNjgzMDA1LjE2ODAyMzYxMDA.*_ga_CW55HF8NVT*MTY4NTQyNTY3NC4zLjEuMTY4NTQyNTc2My4wLjAuMA..'},
+    {id:2,name: 'Zapatillas nike negras', price: 1000, image:'https://firebasestorage.googleapis.com/v0/b/app-keepers.appspot.com/o/image-PhotoRoom%207.png?alt=media&token=6ea0c1a8-4f4f-454d-8a01-8358012f4f63&_gl=1*1wkznzv*_ga*MTMyNjgzMDA1LjE2ODAyMzYxMDA.*_ga_CW55HF8NVT*MTY4NTQyNTY3NC4zLjEuMTY4NTQyNTc2My4wLjAuMA..'},
+    {id:3,name: 'Zapatillas nike negras', price: 1000, image:'https://firebasestorage.googleapis.com/v0/b/app-keepers.appspot.com/o/image-PhotoRoom%207.png?alt=media&token=6ea0c1a8-4f4f-454d-8a01-8358012f4f63&_gl=1*1wkznzv*_ga*MTMyNjgzMDA1LjE2ODAyMzYxMDA.*_ga_CW55HF8NVT*MTY4NTQyNTY3NC4zLjEuMTY4NTQyNTc2My4wLjAuMA..'},
+    {id:4,name: 'Zapatillas nike negras', price: 1000, image:'https://firebasestorage.googleapis.com/v0/b/app-keepers.appspot.com/o/image-PhotoRoom%207.png?alt=media&token=6ea0c1a8-4f4f-454d-8a01-8358012f4f63&_gl=1*1wkznzv*_ga*MTMyNjgzMDA1LjE2ODAyMzYxMDA.*_ga_CW55HF8NVT*MTY4NTQyNTY3NC4zLjEuMTY4NTQyNTc2My4wLjAuMA..'},
+]
 
 //<Home/>
 const ViewHome =()=>{
+    const navigate = useNavigate()
+    const go = (ruta) => {
+        navigate(ruta)
+    }
+    const seeDetailsHandler = () => {
+        alert('Shoop')
+    }
     return(
         <div>
             <Home/>
-            <div className='h-[70vh] w-auto'></div>
+            <div className='h-[60vh] w-auto'></div>
 
-            <div className='h-[70vh] w-auto'>
-                <h2>New styles just arrive</h2>
-                <div className='flex flex-row gap-4 justify-center items-center'>
-                    <CardsProduct/>
-                    <CardsProduct/>
-                    <CardsProduct/>
-                    <CardsProduct/> 
+            <div className='h-[70vh] w-auto flex flex-col justify-center items-center mx-[60px]'>
+                <div className='h-40 w-full flex justify-start'>
+                    <h2 className='text-[40px] text-white'>
+                        New <strong className='text-teal-400'> styles </strong> just arrive
+                    </h2>
+                </div>
+                <div className='flex flex-row gap-4 justify-center items-center overflow'>
+                    {products.map((product, index) => {
+                        return(
+                            <CardsProduct 
+                                key={product.id}
+                                name={product.name} 
+                                price={product.price} 
+                                image={product.image} 
+                                addToCart={seeDetailsHandler} 
+                                seeDetails={() => go(`/product/${product.id}`)}  
+                            />
+                        )
+                    })}
                 </div>
             </div>
             
