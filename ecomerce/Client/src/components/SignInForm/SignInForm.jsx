a79d485bf525a4b837e9fa8999aced4b95334cae
katherine-j-c-s <kathijcs@gmail.com>
2023-05-31 18:15:58 -0300

    almost finished the design of sngIn

--- ecomerce/Client/src/components/SignInForm/SignInForm.jsx
+++ ecomerce/Client/src/components/SignInForm/SignInForm.jsx
@@ -44,41 +44,49 @@ export default function SignInForm() {
         alert('listo')
     }
     return(
-        <div className='text-black'>
-            <div>
+        <div className='flex justify-between text-white'>
+            <div className='bg-black w-2/5'>
                 <Link to={'/'}>
-                    <p className='text-black'>Go Back</p>
+                    <p className='text-white'>Go Back</p>
                 </Link>
-                <h2>Sign In</h2>
-                <form onSubmit={handleSubmit}>
-                    <label>Email </label>
-                    <input
-                        name="email"
-                        value={inputs.email}
-                        onChange={handleChange}
-                        placeholder=""
-                    ></input>
-                    <p>{errors.email}</p>
-                    <label>Password </label>
-                    <input
-                        name="password"
-                        value={inputs.password}
-                        onChange={handleChange}
-                        placeholder=""
-                    ></input>
-                    <p>{errors.password}</p>
-                    
-                    <p className='text-black'>Me olvide mi contraseña</p>
-                    
-                    {Object.keys(errors).length === 0 ? (
-                    <button type="submit">Ingresar</button>
-                    ) : null}
-                </form>
-                <Link to={'/signUp'}>
-                    <p className='text-black'>Sign Up</p>
-                </Link>
+                <div className='flex flex-col align-middle justify-center h-screen '>
+                    <h2 className='mb-20'>Sign In</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className='relative w-fit mb-6'>
+                            <label className='absolute w-16 bg-black h-fit bottom-16 left-8'>Email </label>
+                            <input
+                                className='text-white m-2 border border-cyan-400 bg-transparent rounded-md p-2 pr-20 pl-4'
+                                name="email"
+                                value={inputs.email}
+                                onChange={handleChange}
+                                placeholder=""
+                            ></input>
+                        </div>
+                        <p className='text-rose-500 relative bottom-0'>{errors.email}</p>
+                        <div className='relative w-fit'>
+                            <label className='absolute w-16 bg-black h-fit bottom-16 left-8'>Password </label>
+                            <input
+                                className='text-white m-2 border border-cyan-400 bg-transparent rounded-md p-1 pr-20 pl-4'
+                                name="password"
+                                value={inputs.password}
+                                onChange={handleChange}
+                                placeholder=""
+                            ></input>
+                        </div>
+                        <p className='text-rose-500'>{errors.password}</p>
+                        <p className='text-cyan-400 underline'>Me olvide mi contraseña</p>
+        
+                        {Object.keys(errors).length === 0 ? (
+                        <button type="submit">Ingresar</button>
+                        ) : null}
+                    </form>
+                    <Link to={'/signUp'}>
+                        <p className='text-white'>Sign Up</p>
+                    </Link>
+                </div>
+                
             </div>
-            <div className='bg-cyan-500 w-52 h-52'>
+            <div className='flex w-3/5 bg-cyan-500  h-screm'>
                 <div>
                     <div>
                         <p>“</p>
