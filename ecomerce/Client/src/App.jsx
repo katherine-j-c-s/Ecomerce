307e6f29eb18e5e43f9946a19b78f02a9abf2e9f
Gino Tapia <destroctorgt@gmail.com>
2023-05-31 15:29:09 -0500

    feat: sidebar created

--- ecomerce/Client/src/App.jsx
+++ ecomerce/Client/src/App.jsx
@@ -1,13 +1,17 @@
 import ViewHome from "./views/Home/ViewHome";
 import Nav from "./components/Nav/Nav";
 import { Routes, Route } from "react-router-dom";
+import SideBarCar from "./components/sideBarCar/SideBarCar";
 
 import "./App.css";
 
 function App() {
   return (
     <>
+      {/* <SideBarCar /> */}
+
       <Nav />
+
       <Routes>
         <Route path="/" element={<ViewHome />}></Route>
         <Route path="/women" element={<h1>women</h1>}></Route>
@@ -16,8 +20,10 @@ function App() {
         <Route path="/alls" element={<h1>alls</h1>}></Route>
         <Route path="/signin" element={<h1>sign in</h1>}></Route>
         <Route path="/signup" element={<h1>sign up</h1>}></Route>
-        <Route path="/product/:id" element={<h1>Detalles del producto</h1>}></Route>
-
+        <Route
+          path="/product/:id"
+          element={<h1>Detalles del producto</h1>}
+        ></Route>
       </Routes>
     </>
   );
