e929f78a1b1b32a4d85ed2569861e1300a4e25bd
katherine-j-c-s <kathijcs@gmail.com>
2023-05-31 23:22:08 -0300

    added navegate to signIn an Up to home

--- ecomerce/Client/src/components/SignInForm/SignInForm.jsx
+++ ecomerce/Client/src/components/SignInForm/SignInForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function SignInForm() {
+    const navigate = useNavigate()
     const [inputs, setInputs] = useState({
         email: "",
         password: "",
@@ -50,6 +51,7 @@ export default function SignInForm() {
                 password: "",
             });
             alert('Bienvenido!')
+            navigate("/");
         }
     }
     return(
