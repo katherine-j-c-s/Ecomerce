a81b91e90887203c8a008346db45d6e386886470
katherine-j-c-s <kathijcs@gmail.com>
2023-06-02 00:26:07 -0300

    finished products page  and product form its in progress

--- ecomerce/Client/src/views/Admin/Admin.jsx
+++ ecomerce/Client/src/views/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SidebarAdmin from '../../components/SidebarAdmin/SidebarAdmin'
 import { useLocation } from 'react-router-dom'
 import ProductsAdmin from '../../components/ProductsAdmin/ProductsAdmin'
+import CreateProduct from '../../components/CreateProduct/CreateProduct'
 
 export default function Admin() {
     let {search} = useLocation()
@@ -14,6 +15,7 @@ export default function Admin() {
         {query === 'estadisticas' ? <h2 className='text-black'>{query}</h2> :null}
         {query === 'envios' ? <h2 className='text-black'>{query}</h2> :null}
         {query === 'productos' ? <ProductsAdmin/> :null}
+        {query === 'formProducts' ? <CreateProduct/> :null}
     </div>
   )
 }
