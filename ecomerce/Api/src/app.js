06186371e019c7e7b765cddf2cf6d4dc66e2e39f
Agustin <agustinandrada1@gmail.com>
2023-06-04 20:39:06 -0300

    cambio corse

--- ecomerce/Api/src/app.js
+++ ecomerce/Api/src/app.js
@@ -18,13 +18,13 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
