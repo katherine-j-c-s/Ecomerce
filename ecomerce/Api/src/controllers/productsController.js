e96db3e9e4c8527721583535f827cd0fba998e69
JMCode97 <doomed2506@gmail.com>
2023-06-06 09:29:35 -0400

    cloudinary config file & example env

--- ecomerce/Api/src/controllers/productsController.js
+++ ecomerce/Api/src/controllers/productsController.js
@@ -1,15 +1,8 @@
 const { Product, Color, Category, Size } = require("../db");
 const { hard } = require("./mockedData/mockedProducts");
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("../cloudinary")
 require("dotenv").config();
-const { CLOUD_NAME, API_KEY, API_SECRET } = process.env
 
-cloudinary.config({ 
-  cloud_name: CLOUD_NAME, 
-  api_key: API_KEY, 
-  api_secret: API_SECRET,
-  secure: true
-});
 
 const convertAllProducts = (res) => {
   let { id, name, price, image, Color, Size, Category } = res;
