1c7c456c55914f0874ab18bc48d50229349c934c
katherine-j-c-s <kathijcs@gmail.com>
2023-06-06 16:44:26 -0300

    edit form fixed

--- ecomerce/Client/src/components/createProduct/createProduct.jsx
+++ ecomerce/Client/src/components/createProduct/createProduct.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import swal from 'sweetalert';
+import edit from '../../assets/edit.png'
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addProduct } from '../../redux/actions';
@@ -316,12 +317,8 @@ const CreateProduct = ()=>{
                                             </div>
                                             <div>
                                                 <ul className="flex flex-row items-center gap-2">
-                                                    <li className='relative z-10' onClick={editType}>
-                                                        <button type='reset'  className="p-1.5 bg-bluey rounded-full">
-                                                            <svg onClick={editType} id={`${t.id},${t.color},${t.talla},${t.cantidad}`}  width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000" >
-                                                                <path d="M14.363 5.652l1.48-1.48a2 2 0 012.829 0l1.414 1.414a2 2 0 010 2.828l-1.48 1.48m-4.243-4.242l-9.616 9.615a2 2 0 00-.578 1.238l-.242 2.74a1 1 0 001.084 1.085l2.74-.242a2 2 0 001.24-.578l9.615-9.616m-4.243-4.242l4.243 4.242"stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" ></path>
-                                                            </svg>
-                                                        </button>
+                                                    <li  className='relative z-10 p-2 bg-sky-500 rounded-full'>
+                                                        <img onClick={editType} id={`${t.id},${t.color},${t.talla},${t.cantidad}`} className=' h-3.5  w-3.5' src={edit} alt="" />
                                                     </li>
                                                     <li onClick={deleteType}>
                                                         <button type='reset' id={t.id} className="p-1.5 border-bluey rounded-full"> 
