96d64b9c526a3de0b15395736cc0bbe13a10a969
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-06-07 11:19:00 -0300

    Fix logout

--- ecomerce/Api/src/handlers/usersHandlers.js
+++ ecomerce/Api/src/handlers/usersHandlers.js
@@ -54,8 +54,12 @@ const loginHandler = async (req, res) => {
 
 const logoutHandler = async (req, res) => {
   try {
-    req.logout(); // Cierra la sesión actual del usuario
-    res.status(200).json({ message: "Sesión cerrada exitosamente" });
+    req.logout((err) => {
+      if (err) {
+        throw new Error("Error al cerrar la sesión");
+      }
+      res.status(200).json({ message: "Sesión cerrada exitosamente" });
+    });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
