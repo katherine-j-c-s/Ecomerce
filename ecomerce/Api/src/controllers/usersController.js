287fb5d4aa01a99c2af29c931902cb7efd14a3cd
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-06-07 16:47:54 -0300

    Ruta redirect actualizada + id

--- ecomerce/Api/src/controllers/usersController.js
+++ ecomerce/Api/src/controllers/usersController.js
@@ -103,8 +103,13 @@ const updateUser = async (id, datos) => {
       };
     }
 
+    let hashedPassword = null;
+    if (password) {
+      hashedPassword = bcrypt.hashSync(password, 10); // Encripta la contraseña
+    }
+
     user.mail = mail || user.mail;
-    user.password = password || user.password;
+    user.password = hashedPassword || user.password;
     user.first_name = first_name || user.first_name;
     user.last_name = last_name || user.last_name;
     user.address = address || user.address;
