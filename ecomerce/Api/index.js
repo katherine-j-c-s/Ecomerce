58993e9d84ca7ab23aecfedb2dcef9851ce9610b
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-06-07 11:20:33 -0300

    alter

--- ecomerce/Api/index.js
+++ ecomerce/Api/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
 conn
-  .sync({ force: true })
+  .sync({ alter: true })
   .then(() => {
     app.listen(port, () => {
       console.log(`Server corriendo en el puerto ${port}`); // eslint-disable-line no-console
