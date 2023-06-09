34834e559cd6fa0eb353d6f15cc60e34428cabc1
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-06-09 11:58:50 -0300

    cors

--- ecomerce/Api/src/app.js
+++ ecomerce/Api/src/app.js
@@ -18,20 +18,7 @@ app.use(morgan("dev"));
 
 app.use(bodyParser.json({ limit: "50mb" }));
 
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://sportwear.vercel.app",
-      "http://localhost:3001",
-    ],
-
-
-    credentials: true,
-    methods: "GET, POST, OPTIONS, PUT, DELETE",
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
-  }));
+app.use(cors());
 
 require("./auth")(passport); // Importa y configura la autenticación
 
