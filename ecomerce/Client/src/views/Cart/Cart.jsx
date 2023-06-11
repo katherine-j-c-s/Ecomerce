3436173b8d5babe7a1fa81f0a41364af31f47e45
DVRKSKY <amadeo21sept@gmail.com>
2023-06-11 17:11:23 -0500

    backbutton

--- ecomerce/Client/src/views/Cart/Cart.jsx
+++ ecomerce/Client/src/views/Cart/Cart.jsx
@@ -4,14 +4,20 @@ import CartProducts from '../../components/CartProducts/CartProducts'
 import axios from 'axios'
 import Flags from '../../components/Flags/Flags'
 import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 export default function Cart() {
     
+    const navigate = useNavigate()
+
     const productsCart = useSelector(state => state.sideBarCar)
     const [saveInfo, setSaveInfo] = useState(false)
 
     const [isOpen, setIsOpen] = useState(false)
     const toggleIsOpen = () => setIsOpen(prevState => !prevState)
     
+    const goback = () => {
+        navigate('/alls')
+    }
     const [payForm, setPayForm] = useState({
         first_name:'',
         last_name: '',
@@ -137,6 +143,11 @@ export default function Cart() {
                         </svg>
                         ${productsCart.total}
                     </button>
+                </div>
+                <div className='flex flex-row w-full justify=start items-center mb-10 text-cyan-600 cursor-pointer' onClick={()=> goback()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                    </svg>
                 </div>
                 <h1 className='mb-12 mt-6 text-[#8D8D8D] text-3xl'>Información de <strong>envío</strong></h1>
                 <div className='flex md:flex-row flex-col w-full'>
