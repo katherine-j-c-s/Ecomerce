cf8cd74ee3c8b8e24e2cc21bf7b350dd792e14c9
DVRKSKY <amadeo21sept@gmail.com>
2023-06-12 13:48:12 -0500

    fix(client/cart):solved image in cart4

--- ecomerce/Client/src/components/CartProducts/CartProducts.jsx
+++ ecomerce/Client/src/components/CartProducts/CartProducts.jsx
@@ -4,8 +4,7 @@ export default function CartProducts({image, price, name, quantity}) {
   return (
     <div className='flex flex-row h-24 w-full justify-between my-4'>
         <div className='md:w-24 w-14 md:h-24 h-14 rounded bg-white relative'>
-            <img className='object-cover w-full h-full ' src={image} alt={name} />
-            <span>{image}</span>
+            <img className='object-cover w-full h-full ' src={image.url? image.url : image} alt={name} />
             <div className='w-7 h-7 rounded-full bg-cyan-500 absolute -top-4 -right-4'>
                 <span className='text-white text-xs'>{quantity}</span>
             </div>
