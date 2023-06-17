724760ca10e4b6728a117ffb8b8f8872057af5ed
Gino Tapia <destroctorgt@gmail.com>
2023-06-17 11:54:05 -0500

    fix: se aumento el z-index del carrito y se hizo correciones en el responsive

--- ecomerce/Client/src/components/Nav/Nav.jsx
+++ ecomerce/Client/src/components/Nav/Nav.jsx
@@ -185,7 +185,10 @@ export default function Nav() {
             />
           </Link>
 
-          <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-bluey duration-300">
+          <button
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-bluey duration-300"
+            onClick={() => dispatch(showCart())}
+          >
             <Cart color="#000000" />
           </button>
 
