218280c00295c1ec59d49274a87b195f6c755880
katherine-j-c-s <kathijcs@gmail.com>
2023-06-21 19:00:27 -0300

    add edit function

--- ecomerce/Client/src/components/CardUserAdmin/CardUserAdmin.jsx
+++ ecomerce/Client/src/components/CardUserAdmin/CardUserAdmin.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {getAllUsers, userAdmin, deleteUser} from '../../redux/actions'
+import {getAllUsers, userAdmin, deleteUser,getUserId} from '../../redux/actions'
 import swal from "sweetalert";
 
-import edit from '../../assets/edit.png'
+import editPhoto from '../../assets/edit.png'
 import { useNavigate } from 'react-router-dom'
+import EditUserStatus from '../EditUserStatus/EditUserStatus';
 
 export default function CardUserAdmin() {
     const navegate = useNavigate()
     const dispatch = useDispatch()
 
-    const {allUsers} = useSelector(st=>st)
-
+    const {allUsers,userData} = useSelector(st=>st)
+    let [stado,setStado]= useState(false)
     useEffect(()=>{
         dispatch(getAllUsers())
     },[])
+
     function handleDelete(id) {
         swal({
           title: "Eliminar cuenta",
@@ -37,7 +39,46 @@ export default function CardUserAdmin() {
             });
           }
         });
-      }
+    }
+    const handleEdit = (id) => {
+        dispatch(getUserId(id))
+        if (userData?.id === Number(id)) {
+            if (userData?.role === 'admin') {
+                console.log(userData?.role);
+                setStado('admin')
+            }else if (userData?.role === 'client') {
+                console.log(userData?.role);
+                setStado('Cliente')
+            }else if (user?.role !== 'admin' & user?.role !== 'client') {
+                setStado('')
+            }
+            console.log(userData?.role);
+            swal({
+                title: `${userData?.role === '' ? "Activar cuenta" : "Desactivar Cuenta"}`,
+                text: "¿Estás seguro de que deseas continuar?",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            }).then((confirm) => {
+            if (confirm) {
+                console.log('fue editado correctamente').then((success) => {
+                toast.success("Proceso exitoso.", {
+                    duration: 2000,
+                });
+        
+                setTimeout(() => {
+                    toast.remove();
+                    dispatch(logOut());
+                    navegate("/admin?pestaña=usuarios");
+                }, 2000);
+                });
+            }
+            });
+        }
+    }
+    const closeEdit = () => {
+        setEdit({show:false,id:null,user:null})
+    }
     const userDetail = (e) =>{
         let id = e.target.id.split('/')
         let user = allUsers.find(u => u.id === Number(id[0]))
@@ -45,7 +86,7 @@ export default function CardUserAdmin() {
         navegate('/admin?pestaña=usuarioDetail')
     }
   return (
-    <div className='mt-16 h-fit ml-1 md:ml-10 dark'>
+    <div className='mt-16 h-fit ml-1 md:ml-10 dark relative'>
         <h1 className='text-slate-400 mb-8 text-xl font-bold'>Usuarios</h1>
         <div className='flex flex-wrap'>
             {allUsers?.map(u=>{
@@ -54,11 +95,11 @@ export default function CardUserAdmin() {
                         <div className='mx-6'>
                             <img className='h-10 w-10 rounded-full' src={u.image.url} alt="vector" />
                         </div>
-                        <p onClick={userDetail} id={`${u.id}/usuarioDetail`} className='mr-8 mt-3 font-bold hover:text-lg cursor-pointer dark:hover:text-slate-300 hover:text-sky-400 transition-all mx-5'>{u.first_name}</p>
+                        <p onClick={userDetail} id={`${u.id}/usuarioDetail`} className='mr-8 mt-3 font-bold hover:text-lg cursor-pointer relative dark:hover:text-slate-300 hover:text-sky-400 transition-all z-0 mx-5'>{u.first_name}</p>
                         <div className='flex mt-2'>
-                            {/* <div className='relative mr-2 z-10 cursor-pointer border hover:border-black border-solid transition-all w-fit h-fit p-2.5 bg-sky-500 rounded-full'>
-                                <img onClick={userDetail} id={`${u.id}/editUser`} className='h-3  w-3' src={edit} alt="" />
-                            </div> */}
+                            <div onClick={()=>handleEdit(u.id)} className='relative mr-2 z-10 cursor-pointer hover:border-black transition-all w-fit h-fit p-2.5 bg-sky-500 rounded-full'>
+                                <img className='h-3 w-3 relative z-10' src={editPhoto} alt="" />
+                            </div>
                             <div onClick={()=>handleDelete(u.id)} className="p-1.5 mr-3 border-bluey dark:border-none dark:bg-sky-400 dark:hover:bg-sky-200 cursor-pointer w-fit hover:bg-bluey transition-all h-fit border rounded-full">
                                 <svg width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
                                     <path d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" ></path>
