77254983d23728f3a219cb50d56f526e77e6f3da
katherine-j-c-s <kathijcs@gmail.com>
2023-06-22 12:46:36 -0300

    add

--- ecomerce/Client/src/views/Profile/Profile.jsx
+++ ecomerce/Client/src/views/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import edit from "../../assets/edit.png";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import swal from "sweetalert";
+import Typewriter from "typewriter-effect";
 
 import {
   getProductById,
@@ -287,9 +288,13 @@ export default function Profile() {
   return (
     <main className={!darkModeClient ? '' : 'dark'}>
         <div className="dark:bg-slate-950 dark:text-slate-300 bg-slate-300 text-black w-full flex md:flex-row flex-col justify-center relative h-fit md:h-screen">
-        <h2 className="absolute md:top-8 top-56 text-lg w-full font-bold mx-auto md:w-fit">
-          {"Hola" + " " + user.name + "!"}
-        </h2>
+        <div className="absolute md:top-8 top-56 text-lg w-full font-bold mx-auto md:w-fit">
+          <Typewriter onInit={(typewriter)=> {
+            typewriter.typeString(`${"Hola" + " " + user.name + "!"}`)
+            .start();
+            }}
+          />
+        </div>
         <div>
           <Toaster />
         </div>
