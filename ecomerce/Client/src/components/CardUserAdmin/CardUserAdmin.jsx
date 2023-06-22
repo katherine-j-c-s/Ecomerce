dcda5be93271078376e7d220eac0b4497ae52856
Agustin <agustinandrada1@gmail.com>
2023-06-22 00:07:48 -0300

    asd

--- ecomerce/Client/src/components/CardUserAdmin/CardUserAdmin.jsx
+++ ecomerce/Client/src/components/CardUserAdmin/CardUserAdmin.jsx
@@ -82,9 +82,9 @@ export default function CardUserAdmin() {
                         </div>
                         <p onClick={userDetail} id={`${u.id}/usuarioDetail`} className='mr-8 mt-3 font-bold hover:text-lg cursor-pointer relative dark:hover:text-slate-300 hover:text-sky-400 transition-all z-0 mx-5'>{u.first_name}</p>
                         <div className='flex mt-2'>
-                            <div onClick={()=>handleEdit(u.id)} className='relative mr-2 z-10 cursor-pointer hover:border-black transition-all w-fit h-fit p-2.5 bg-sky-500 rounded-full'>
+                            <button onClick={()=>handleEdit(u.id)} className='relative mr-2 z-10 cursor-pointer hover:border-black transition-all w-fit h-fit p-2.5 bg-sky-500 rounded-full'>
                                 <img className='h-3 w-3 relative z-10' src={editPhoto} alt="" />
-                            </div>
+                            </button>
                             <div onClick={()=>handleDelete(u.id)} className="p-1.5 mr-3 border-bluey dark:border-none dark:bg-sky-400 dark:hover:bg-sky-200 cursor-pointer w-fit hover:bg-bluey transition-all h-fit border rounded-full">
                                 <svg width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
                                     <path d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75"stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" ></path>
